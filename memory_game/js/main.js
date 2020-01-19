44007f06dd81b06ef3713ecd5eff0e3e8dbb026f
Patrick White <patrick.cf.white@gmail.com>
2020-01-19 19:24:48 +0000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "king";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
