6661e6f108e1b7fb9389dbf3612211e1669fcd8a
Patrick White <patrick.cf.white@gmail.com>
2020-01-20 22:46:05 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,24 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    }, 
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    }, 
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }];
 let cardsInPlay = [];
 function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,8 +28,10 @@ function checkForMatch() {
     }
 }
 function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
