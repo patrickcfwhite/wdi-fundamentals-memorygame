b143001e671363e1538bbf5abe279400397dc5a7
Patrick White <patrick.cf.white@gmail.com>
2020-01-20 21:28:57 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,14 +1,18 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[1];
-let cardTwo = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " +  cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!")
+        console.log("You found a match!");
     } else {
-        alert("Sorry, try again.")
+        console.log("Sorry, try again.");
     }
 }
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+};
+flipCard(0);
+flipCard(2);
